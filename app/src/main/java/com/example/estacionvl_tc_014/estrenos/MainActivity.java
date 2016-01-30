6a33dce4403a9d403f387ad0bec6fb387126e98c
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-01-30 12:15:52 -0500

    Implementado ListView - Simple

--- app/src/main/java/com/example/estacionvl_tc_014/estrenos/MainActivity.java
+++ app/src/main/java/com/example/estacionvl_tc_014/estrenos/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.estacionvl_tc_014.estrenos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView list;
+    String peliculas[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list);
+
+        list.setOnItemClickListener(this);
+        peliculas = getResources()
+                .getStringArray(R.array.peliculas);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this,"Seleccionaste "+peliculas[position]
+                , Toast.LENGTH_SHORT)
+                .show();
     }
 }
---
+++ app/src/main/java/com/example/estacionvl_tc_014/estrenos/models/Pelicula.java
@@ -0,0 +1,53 @@
+package com.example.estacionvl_tc_014.estrenos.models;
+
+/**
+ * Created by EstacionVL-TC-014 on 30/01/16.
+ */
+public class Pelicula {
+
+    String titulo;
+    String duracion;
+    float calificacion;
+    String descripcion;
+    String url;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public float getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(float calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
